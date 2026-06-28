b7aa149a907509c131f7674519166a6943606e9e
Sebzep <sebastianzepke@gmail.com>
2026-06-28 17:00:30 +0200

    refactor
    refactor: use setUp method in tests to avoid duplication (REFACTOR)

--- TST-E1/A2/test_shopping_cart.py
+++ TST-E1/A2/test_shopping_cart.py
@@ -1,19 +1,14 @@
 import unittest
-# Wir importieren die Klasse aus der anderen Datei
-from Shoppingcart import ShoppingCart
+from shopping_cart import ShoppingCart
 
 class TestShoppingCart(unittest.TestCase):
-    
-    # Das hier läuft automatisch VOR jedem einzelnen Test
-    # So starten wir immer mit einem frischen, leeren Warenkorb
+    # Läuft vor jedem einzelnen Test und erstellt einen frischen Warenkorb
     def setUp(self):
         self.cart = ShoppingCart()
 
-    # Test für Feature 1: Der leere Warenkorb
     def test_new_cart_is_empty(self):
         self.assertEqual(self.cart.get_count(), 0)
 
-    # Test für Feature 2: Produkt hinzufügen
     def test_add_product_increases_count(self):
         self.cart.add_product("Apfel")
         self.assertEqual(self.cart.get_count(), 1)
