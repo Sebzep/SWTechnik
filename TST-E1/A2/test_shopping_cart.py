ff03b9911cc945ac15f0b8483d2f558e2060bc65
Sebzep <sebastianzepke@gmail.com>
2026-06-28 16:27:18 +0200

    Red test for adding a product
    feat: add test for adding a product (RED)

--- TST-E1/A2/test_shopping_cart.py
+++ TST-E1/A2/test_shopping_cart.py
@@ -1,12 +1,22 @@
 import unittest
-# Wir versuchen, eine Klasse zu importieren, die es noch nicht gibt
-from shopping_cart import ShoppingCart
+# Wir importieren die Klasse aus der anderen Datei
+from Shoppingcart import ShoppingCart
 
 class TestShoppingCart(unittest.TestCase):
+    
+    # Das hier läuft automatisch VOR jedem einzelnen Test
+    # So starten wir immer mit einem frischen, leeren Warenkorb
+    def setUp(self):
+        self.cart = ShoppingCart()
+
+    # Test für Feature 1: Der leere Warenkorb
     def test_new_cart_is_empty(self):
-        cart = ShoppingCart()
-        # Der Warenkorb sollte am Anfang 0 Produkte enthalten
-        self.assertEqual(cart.get_count(), 0)
+        self.assertEqual(self.cart.get_count(), 0)
+
+    # Test für Feature 2: Produkt hinzufügen
+    def test_add_product_increases_count(self):
+        self.cart.add_product("Apfel")
+        self.assertEqual(self.cart.get_count(), 1)
 
 if __name__ == '__main__':
     unittest.main()
