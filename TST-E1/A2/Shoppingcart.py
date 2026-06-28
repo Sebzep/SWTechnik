b9be45d584e457a42513071cea470e3787497034
Sebzep <sebastianzepke@gmail.com>
2026-06-28 16:29:21 +0200

    Green 2
    feat: implement product list and add method (GREEN)

--- TST-E1/A2/Shoppingcart.py
+++ TST-E1/A2/Shoppingcart.py
@@ -1,4 +1,12 @@
 class ShoppingCart:
+    def __init__(self):
+        # Wir erstellen eine Liste für die Produkte
+        self.items = []
+
+    def add_product(self, product):
+        # Produkt in die Liste packen
+        self.items.append(product)
+
     def get_count(self):
-        # Hartcodiert, damit der Test gerade so besteht
-        return 0
+        # Jetzt geben wir die echte Länge der Liste zurück
+        return len(self.items)
